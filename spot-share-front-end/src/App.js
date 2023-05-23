002e7e6c99bece8b577362bbfe5bbe1a623b2389
xiangyi-zhou <zxyzhouxiangyi@gmail.com>
2023-05-23 14:47:13 +0100

    added and styled header, navLinks, sideDrawer

--- spot-share-front-end/src/App.js
+++ spot-share-front-end/src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" exact>
-        <Users />
-      </Route>
-      <Route path='/places/new'>
-        <NewPlace />
-      </Route>
+      <MainNavigation />
+      <main>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+      </main>
     </Router>
   );
 };
--- spot-share-front-end/src/user/compenents/UsersList.js
+++ spot-share-front-end/src/user/compenents/UsersList.js
@@ -1,20 +1,23 @@
 import React from "react";
 
 import UserItem from "./UserItem";
-import './UsersList.css'
+import "./UsersList.css";
+import Card from "../../shared/components/UIElements/Card";
 
 const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
 
   return (
     <ul className="users-list">
-      {props.items.map(user => (
+      {props.items.map((user) => (
         <UserItem
           key={user.id}
           id={user.id}
