5438f61c3be305ecd1edb336342ac90071966aa7
xiangyi-zhou <zxyzhouxiangyi@gmail.com>
2023-05-30 20:25:34 +0100

    added POST route, and fixed uuid issue

--- spot-share-back-end/app.js
+++ spot-share-back-end/app.js
@@ -5,14 +5,16 @@ const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use('/api/places', placesRoutes);
+app.use(bodyParser.json());
+
+app.use('/api/places', placesRoutes); // => /api/places...
 
 app.use((error, req, res, next) => {
-    if(res.headerSent){
-        return next(error);        
-    }
-    res.status(error.code || 500);
-    res.json({message: error.message || 'An unknown error occurred!'});
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
 });
 
 app.listen(5000);
--- spot-share-back-end/routes/places-routes.js
+++ spot-share-back-end/routes/places-routes.js
@@ -4,8 +4,10 @@ const placesControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
 
-router.get('/:pid',placesControllers.getPlaceById);
+router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlaceByUserId);
+
+router.post('/', placesControllers.createPlace);
 
 module.exports = router;
