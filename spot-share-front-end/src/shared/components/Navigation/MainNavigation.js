9ff830d11aa15d45e209948fbc6cb8c28f7cece1
xiangyi-zhou <zxyzhouxiangyi@gmail.com>
2023-05-30 13:27:22 +0100

    uploaded modified components

--- spot-share-front-end/src/shared/components/Navigation/MainNavigation.js
+++ spot-share-front-end/src/shared/components/Navigation/MainNavigation.js
@@ -1,34 +1,37 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import MainHeader from "./MainHeader";
-import NavLinks from "./NavLinks";
-import SideDrawer from "./SideDrawer";
-import Backdrop from "../UIElements/Backdrop";
-import "./MainNavigation.css";
+import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
+import './MainNavigation.css';
 
-
-const MainNavigation = (props) => {
+const MainNavigation = props => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-  const openDrawer = () => {
-      setDrawerIsOpen(true);
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
   };
 
-  const closeDrawer = () => {
+  const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
   };
+
   return (
     <React.Fragment>
-      {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
-      <SideDrawer show={drawerIsOpen}>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
-    
+
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
--- spot-share-front-end/src/user/pages/Users.js
+++ spot-share-front-end/src/user/pages/Users.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import UsersList from "../compenents/UsersList";
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = 
-    [{
-        id: 'ul', 
-        name: 'Max Schwarz', 
-        image: 'https://cdn2.vectorstock.com/i/1000x1000/20/76/man-avatar-profile-vector-21372076.jpg',
-        places: 3
-    }];
-    return <UsersList items={USERS}/>;
-}
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Max Schwarz',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
